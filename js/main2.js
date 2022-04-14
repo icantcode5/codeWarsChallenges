c115df4c0d11f8d1174584414387ce440778d2c2
Carlos Sosa <carlossosa54321@gmail.com>
2022-04-14 13:23:45 -0700

    added more solutions

--- js/main2.js
+++ js/main2.js
@@ -132,4 +132,23 @@ function DNAStrand(dna){
     return dna.split('').map(function(i) { return code[i] }).join('')
 }
  
-  
+//Return the last 4 numbers of a credit card while all other numbers are masked by '#'s
+
+function maskify(cc) {
+    return cc.slice(-4).padStart(cc.length,'#')
+}
+
+//return the sum of the two smallest integers of the given array
+function sumTwoSmallestNumbers(numbers) {  
+    numbers.sort( (a, b) => a-b)
+    return numbers[0] + numbers[1]
+}
+
+//Find the sum of the range of numbers while only being given the low and high
+
+const GetSum = (a, b) => {
+    let min = Math.min(a, b),
+        max = Math.max(a, b);
+    return (max - min + 1) * (min + max) / 2;
+}
+
