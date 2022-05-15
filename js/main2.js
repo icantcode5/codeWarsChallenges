174b24997c7e2a883ffc995d8aed73c570ebdd9e
Carlos Sosa <carlossosa54321@gmail.com>
2022-05-14 18:58:10 -0700

    more solutions

--- js/main2.js
+++ js/main2.js
@@ -855,6 +855,18 @@ function countDevelopers(list) {
   }
  return count
 }
+
+//In honor of my grandfather's memory we will write a function using his formula!
+// Take a list of ages when each of your great-grandparent died.
+// Multiply each number by itself.
+// Add them all together.
+// Take the square root of the result.
+// Divide by two.
+
+function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
+  let arr = [age1,age2,age3,age4,age5,age6,age7,age8]
+  return Math.floor(Math.sqrt(arr.map(element => element * element).reduce((acc,cur)=> acc+cur,0))/2)
+}
  
 
 
