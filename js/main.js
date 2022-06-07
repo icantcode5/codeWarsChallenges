ecb2cece1b102347f20b713338a0c6684a2aa5c8
Carlos Sosa <carlossosa54321@gmail.com>
2022-06-07 16:43:03 -0700

    sols

--- js/main.js
+++ js/main.js
@@ -155,4 +155,37 @@ function between(a, b) {
   return arr //return the filled array
 }
 
+//Return booleans as a string type
+
+function booleanToString(b){
+  if(b === true){
+    return 'true'
+  }else{
+    return 'false'
+  }
+}
+
+//Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+//Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+function paperwork(n, m) {
+  if(n < 0|| m < 0){
+    return 0
+  }else{
+    return n*m
+  }
+}
+
 //
+
+async function getACuteDogPhoto(){
+  try{
+    function()
+  } catch(error){
+    console.error(error)
+  }
+  const res = await fetch('https://dog.ceo/api/breeds/image/random')
+  const data = await res.json()
+  console.log(data)
+}
+getACuteDogPhoto()
