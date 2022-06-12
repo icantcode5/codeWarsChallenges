f81847542084e53e5d54a24e332af6021046fa94
Carlos Sosa <carlossosa54321@gmail.com>
2022-06-12 16:34:11 -0700

    1 solution

--- js/main.js
+++ js/main.js
@@ -366,7 +366,35 @@ var countSheep = function (num){
   return str
 }
 
-//
+//Create a function with two arguments that will return an array of the first (n) multiples of (x).
 
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+// Return the results as an array (or list in Python, Haskell or Elixir).
+// Examples:
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+function countBy(x, n) {
+  let arr = []
+  let num = x*n
+  for(let i = x;i<=num;i+=x){ //start the loop at the mutiple, go up to the length x*n which is the array.length and finally increment by the given multiple of x
+    arr.push(i)
+  }
+  return arr
+}
+
+//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Return your answer as a number.
+
+function sumMix(x){
+  return x.map(Number).reduce((a,b)=> a + b,0)
+}
+
+//ALSO ANOTHER SOLUTION THAT IS FASTER :
+function sumMix(x){
+  return x.reduce((a,b) => a + +b , 0)
+}
+
+//
 
 
