b116d49574d841b6c14b36f0693ed898439df036
Carlos Sosa <carlossosa54321@gmail.com>
2022-06-11 22:19:55 -0700

    nice solutions

--- js/main.js
+++ js/main.js
@@ -348,7 +348,25 @@ function bonusTime(salary, bonus) {
   return bonus ? '£' + salary *10 : '£' + salary // if bouns is boolean true, multiply salary times 10 else return salary
 }
 
+//return a string as an array
 
+function stringToArray(string){
+  return string.split(' ');
+}
+
+//If you can't sleep, just count sheep!!
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+var countSheep = function (num){
+  let str = ''
+ for(let i = 1;i<=num;i++){
+   str += `${i} sheep...`
+ }
+  return str
+}
+
+//
 
 
 
